79b65ebc4c016d6713c92618031a549e82f2c96e
Henrik Bråthen <hsbrathen1@gmail.com>
2021-04-27 21:28:44 +0200

    extract auth to its own file (REVERT THIS IF NOT WORKING)

---
+++ src/server/authApi.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const authApi = express.Router();
+const session = require("express-session");
+
+authApi.use(
+  session({
+    secret: "32bJS7s5k5al",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+const users = [
+  {
+    id: 1,
+    firstName: "Per",
+    lastName: "Borgli",
+    email: "per@borgli",
+    password: "123",
+  },
+  {
+    id: 2,
+    firstName: "Sander",
+    lastName: "Nilsen",
+    email: "sander@nilsen",
+    password: "123",
+  },
+];
+
+authApi.get("/api/user", (req, res) => {
+  console.log(users);
+  res.json(users);
+});
+
+authApi.post("/api/user", (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  console.log(req.body);
+  users.push({ firstName, lastName, email, password, id: users.length + 1 });
+  console.log(users);
+  res.status(201).end();
+});
+
+authApi.get("/api/profile", (req, res) => {
+  const { username } = req.session;
+  if (!username) {
+    return res.status(401).send();
+  }
+  res.json({ username });
+});
+
+authApi.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+
+  let foundUser = false;
+  let user;
+  users.forEach((e) => {
+    if (e.email == username && e.password == password) {
+      foundUser = true;
+      user = e;
+    }
+  });
+
+  if (foundUser) {
+    req.session.username = username;
+    res.end();
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+module.exports = authApi;
--- src/server/server.js
+++ src/server/server.js
@@ -1,81 +1,15 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
 const messageApi = require("./messageApi");
+const authApi = require("./authApi");
 
 const app = express();
 
-app.use(
-  session({
-    secret: "32bJS7s5k5al",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
 app.use(bodyParser.json());
-app.use(cookieParser());
 
 app.use("/api/message", messageApi);
-
-const users = [
-  {
-    id: 1,
-    firstName: "Per",
-    lastName: "Borgli",
-    email: "per@borgli",
-    password: "123",
-  },
-  {
-    id: 2,
-    firstName: "Sander",
-    lastName: "Nilsen",
-    email: "sander@nilsen",
-    password: "123",
-  },
-];
-
-app.get("/api/user", (req, res) => {
-  console.log(users);
-  res.json(users);
-});
-
-app.post("/api/user", (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
-  console.log(req.body);
-  users.push({ firstName, lastName, email, password, id: users.length + 1 });
-  console.log(users);
-  res.status(201).end();
-});
-
-app.get("/api/profile", (req, res) => {
-  const { username } = req.session;
-  if (!username) {
-    return res.status(401).send();
-  }
-  res.json({ username });
-});
-
-app.post("/api/login", (req, res) => {
-  const { username, password } = req.body;
-
-  let foundUser = false;
-  let user;
-  users.forEach((e) => {
-    if (e.email == username && e.password == password) {
-      foundUser = true;
-      user = e;
-    }
-  });
-
-  if (foundUser) {
-    req.session.username = username;
-    res.end();
-  } else {
-    res.sendStatus(401);
-  }
-});
+app.use("", authApi);
 
 app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
 app.use((req, res, next) => {
